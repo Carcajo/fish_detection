47a30836b6eca3e5aaef7e6e962c665f73bf4df6
Maxim Puhov <t375445391507@gmail.com>
2024-08-15 17:33:02 +0300

    fix:delete unnecessary elements

--- scripts/predict_image.py
+++ scripts/predict_image.py
@@ -1,14 +1,3 @@
-# import cv2
-# from ultralytics import YOLO
-#
-# model = YOLO('model/best.pt')
-# img_path = 'rory.jpg'
-# img = cv2.imread(img_path)
-#
-# results = model(img)
-# annotated_img = results[0].plot()
-# cv2.imwrite('path_to_output_image.jpg', annotated_img)
-
 from ultralytics import YOLO
 
 model = YOLO('model/best2.pt')
